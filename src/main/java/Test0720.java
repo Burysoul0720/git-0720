a3759b10d37ed3d54b6b2d474fedd7585dfabec3
Burysoul <Burysoul@atguigu.com>
2020-11-27 15:52:47 +0800

    first commit

---
+++ src/main/java/Test0720.java
@@ -0,0 +1,11 @@
+/**
+ * @author Burysoul
+ * @date 2020/11/27 15:49
+ */
+public class Test0720 {
+    public static void main(String[] args) {
+        System.out.println("测试1");
+        String s = "执行";
+        System.out.println(s);
+    }
+}
